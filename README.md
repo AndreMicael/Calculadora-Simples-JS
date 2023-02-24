71dfa671f02f5a430dec5c2e4e5fbee89f6720ac
André Sampaio <49279300+AndreMicael@users.noreply.github.com>
2023-02-24 12:27:31 -0400

    Add files via upload
    Melhorei o visual da calculadora via CSS e HTML

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 
 A Pen created on CodePen.io. Original URL: [https://codepen.io/andremicael/pen/dyqpoJR](https://codepen.io/andremicael/pen/dyqpoJR).
 
-Calculadora simples criada em HTML, CSS e Javascript
--- src/script.js
+++ src/script.js
@@ -1,20 +1,24 @@
 function outcome() {
-  let num1 = Number(document.querySelector("#num-one").value);
-  let num2 = Number(document.querySelector("#num-two").value);
-  let result = 0; 
-  
-  sum = document.getElementById('box1').checked;
-  subtract = document.getElementById('box2').checked;
-  multiply = document.getElementById('box3').checked;
-  divide = document.getElementById('box4').checked;
-  
-  if (sum) 
-    result = num1 + num2;
-  else if (subtract)
-    result = num1 - num2;
-  else if (multiply)
-    result = num1 * num2;
-  else if(divide)
-    result =  num1 / num2;    
-  document.querySelector("#result").value = result;
+    //Seleciona os valores presentes nos campos para fazer a soma
+    let num1 = Number(document.querySelector("#num-one").value);
+    let num2 = Number(document.querySelector("#num-two").value);
+    let result = 0;
+
+    // Recebe o operador que foi selecionado pelo usuário
+    sum = document.getElementById('box1').checked;
+    subtract = document.getElementById('box2').checked;
+    multiply = document.getElementById('box3').checked;
+    divide = document.getElementById('box4').checked;
+
+    //Faz as operações
+    if (sum)
+        result = num1 + num2;
+    else if (subtract)
+        result = num1 - num2;
+    else if (multiply)
+        result = num1 * num2;
+    else if (divide)
+        result = num1 / num2;
+    //Divulga os resultados e retorna para o campo específico
+    document.querySelector("#result").value = result;
 }
